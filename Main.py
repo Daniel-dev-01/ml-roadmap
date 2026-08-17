8a162ba1f2da007c23ee5fbe933a6fbc3e517d62
Daniel-dev-01 <danielmomoh905@gmail.com>
2026-08-17 16:25:04 +0100

    Add basic EDA visualizations

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import  pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd. read_csv("data.csv")
 
@@ -33,3 +34,35 @@ cols = ["OnlineSecurity", "OnlineBackup", "DeviceProtection", "TechSupport", "St
 df[cols] = df[cols].replace({"No internet service": "No"})
 
 print(df["DeviceProtection"].value_counts())
+
+#1. Churn Distribution
+
+df["Churn"].value_counts().plot(kind="bar")
+plt.title("Customer Churn Distribution")
+plt.xlabel("Churn")
+plt.ylabel("Number of customers")
+plt.show()
+
+#2. Tenure Distribution
+df["tenure"].plot(kind="hist",bins=20)
+plt.title("Customer tenure Distribution")
+plt.xlabel("Tenure(months)")
+plt.ylabel("Number of customers")
+plt.show()
+
+# 3. Monthly Charges by Churn
+df.boxplot(column='MonthlyCharges', by='Churn')
+plt.title('Monthly Charges by Churn')
+plt.suptitle('')
+plt.xlabel('Churn')
+plt.ylabel('Monthly Charges')
+plt.show()
+
+# 4. Churn by Contract Type
+pd.crosstab(df['Contract'], df['Churn']).plot(kind='bar')
+plt.title('Churn by Contract Type')
+plt.xlabel('Contract Type')
+plt.xticks(rotation=0)
+plt.ylabel('Number of Customers')
+plt.show()
+
